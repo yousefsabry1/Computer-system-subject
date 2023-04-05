9782dd6e606898accef97522c14b7617c9f20936
The phoenix <129897128+yousefsabry1@users.noreply.github.com>
2023-04-05 15:11:06 +0300

    Add files via upload

---
+++ Assignment 2/1/find maximum between two numbers using conditional/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int a , b;
+
+    printf("Enter your first number : ");
+    scanf("%d", &a);
+    printf("Enter your second number : ");
+    scanf("%d", &b);
+
+
+
+
+    (a > b) ? printf("%d is larger than %d \n",a,b) : printf("%d is larger than %d \n",b,a);
+
+
+    printf("\033[0;33m");
+ printf("#The phoenix ):\n");
+    printf("\033[0;37m");
+
+
+ printf("Please enter any key to exit .");
+ system("pause > null");
+
+
+
+    return 1;
+
+
+}
---
+++ Assignment 2/4/check whether character is an alphabet or not conditional operator/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+    char ch;
+    printf("Enter your character : ");
+    scanf("%c",&ch);
+
+
+    (ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z') ? printf("%c is an alphabet \n",ch) : printf("%c isn’t an alphabet \n",ch);
+
+    printf("\033[0;33m");
+ printf("#The phoenix ):\n");
+    printf("\033[0;37m");
+
+
+ printf("Please enter any key to exit .");
+ system("pause > null");
+
+
+
+    return 1;
+}
